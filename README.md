3fd64f35899b203a6e5176a7546b9fa77a5f0fdf
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-24 21:35:37 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# http-dump
+
+Listen on a port, dump everything that comes to the filesystem.
+
+```shell
+$ PORT="1337" http-dump
+```
+And follow the on-screen instructions.
+
+# install
+with [npm](https://npmjs.org) do:
+
+```
+npm install http-dump
+```
+
+# license
+
+MIT
---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const dump = require('express-dump');
+
+const port = process.env.PORT || 5000;
+
+const { middleware, dump: flush } = dump({basePath: 'dump'});
+
+app.use(middleware);
+
+app.listen(port);
+console.log(`Collecting logs on port ${port}. Press ^C to shutdown and dump.`);
+
+process.on('SIGINT', function() {
+    console.log('Dumping...');
+    flush()
+        .then(() => console.log('Have a nice day!'))
+        .catch(err => console.error('Could not dump: ', err))
+        .then(() => process.exit());
+});
